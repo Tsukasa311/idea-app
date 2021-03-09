b6daedfe70fe7c7cf6ed2313f9e0f09133630907
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-03-09 18:49:29 +0900

    条件分岐作成

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,17 +1,16 @@
 class IdeasController < ApplicationController
   
   def index
-    ideas = Idea.all
-    data = []
-    ideas.each do |idea|
-      data <<
-      {
-        id: idea.id,
-        category: idea.category.name,
-        body: idea.body
-      }
+    if Category.where(name: params[:name]).exists?
+      category_id = Category.where(name: params[:name]).ids
+      ideas = Idea.where(category_id: category_id)
+      data = make_array(ideas)
+      render json: {data: data}
+    else  
+      ideas = Idea.includes(:category)
+      data = make_array(ideas)
+      render json: {data: data}
     end
-    render json: {data: data}
   end
 
   def create
@@ -28,6 +27,19 @@ class IdeasController < ApplicationController
 
   def category_idea_paramns
     params.require(:category_idea).permit(:name, :body)
+  end
+
+  def make_array(ideas)
+    data_array = []
+    ideas.each do |idea|
+      data_array <<
+      {
+        id: idea.id,
+        category: idea.category.name,
+        body: idea.body
+      }
+    end
+    data_array
   end
 
 end
