e40b2722ac72ec9c52344217cf43f578b1c7ed39
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-03-10 18:20:54 +0900

    indexアクションテストcontext追加

--- spec/requests/ideas_spec.rb
+++ spec/requests/ideas_spec.rb
@@ -6,20 +6,24 @@ describe IdeasController, type: :request do
   end
 
   describe 'GET #index' do
-    it 'category_nameを指定してリクエストすると、該当するcategoryのアイデアを返す' do
-      params = { category_name: @idea.category.name }
-      get ideas_path(params)
-      expect(response.body).to include(@idea.category.name).and include(@idea.body)
+    context 'アイデアが取得できるとき' do
+      it 'category_nameを指定してリクエストすると、該当するcategoryのアイデアを返す' do
+        params = { category_name: @idea.category.name }
+        get ideas_path(params)
+        expect(response.body).to include(@idea.category.name).and include(@idea.body)
+      end
+      it 'category_nameを指定せずにリクエストすると、全てのアイデアを返す' do
+        another_idea = FactoryBot.create(:idea)
+        params = { category_name: nil }
+        get ideas_path(params)
+        expect(response.body).to include(@idea.body).and include(another_idea.body)
+      end
     end
-    it 'category_nameを指定せずにリクエストすると、全てのアイデアを返す' do
-      another_idea = FactoryBot.create(:idea)
-      params = { category_name: nil }
-      get ideas_path(params)
-      expect(response.body).to include(@idea.body).and include(another_idea.body)
-    end
-    it '登録されていないカテゴリーをリクエストすると、ステータスコード404を返す' do
-      get ideas_path(category_name: 'aaa')
-      expect(response.status).to eq(404)
+    context 'アイデアが取得できないとき' do
+      it '登録されていないカテゴリーをリクエストすると、ステータスコード404を返す' do
+        get ideas_path(category_name: 'aaa')
+        expect(response.status).to eq(404)
+      end
     end
   end
 
@@ -40,7 +44,6 @@ describe IdeasController, type: :request do
         end.to change(Idea, :count).by(1).and change(Category, :count).by(1)
       end
     end
-
     context '保存できないとき' do
       it 'リクエストに失敗するとステータスコード422を返す(カテゴリーが空）' do
         params = { category_idea: { category_name: '', body: 'new_body' } }
